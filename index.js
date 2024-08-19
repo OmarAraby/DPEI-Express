842b60aade9fbc67ff5ea9802f4573b242b6e8ce
Omar Araby <omararaby789@gmail.com>
2024-08-19 19:17:00 +0300

    GET Request

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const app = express();
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Hello from express server ${port}`));
+
+//  GET Request ==> app.get
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Get Request" });
+});
+
+//  POST Request ==> app.post
